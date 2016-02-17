66a591f4d763ed07d10dde1380de40f35da04113
yi <yi@yis-iMac.local>
2016-02-17 17:05:07 +0800

    add ssacount view

--- wsgi/myproject/ssacount/views.py
+++ wsgi/myproject/ssacount/views.py
@@ -5,3 +5,13 @@ def index(request):
     return HttpResponse("Hello World, Django")
 def detail(request, my_args):
     return HttpResponse("You're looking at my_args %s." % my_args)
+def acounts(request):
+    return HttpResponse(
+    '''{
+    "local_port": 1080, 
+    "method": "aes-256-cfb", 
+    "password": "44982810", 
+    "server": "us1.iss.tf", 
+    "server_port": "443", 
+    "timeout": 60
+    }''')
