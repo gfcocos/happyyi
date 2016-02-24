1b6289ba9f78c6856b78fd02e8f42d468bf74642
yi <yi@yis-iMac.local>
2016-02-24 19:01:59 +0800

    bugfix nn

--- wsgi/myproject/ssacount/views.py
+++ wsgi/myproject/ssacount/views.py
@@ -18,7 +18,9 @@ def acounts(request):
     if len(acounts) > 0:
         acount = acounts[0]
         if acount.server_aes == '':
-            acount.server_aes = base64.encodestring(acount.server)
+            aes = base64.encodestring(acount.server).replace('\n','');
+            aes = aes.replace(' ','')
+            acount.server_aes = aes
             acount.save()
         t = strftime("%Y-%m-%d %H:%M:%S", gmtime())
         output = ('{ "local_port": %s, "method": "%s", "password": "%s", "server": "%s", "server_aes": "%s", "server_port": "%s" , "time": "%s"}' % (1080, acount.method, acount.password, acount.server, acount.server_aes, acount.server_port,t))
